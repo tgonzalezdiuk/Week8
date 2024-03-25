67f02ed15d10b05df1f7304b304ea92326eb6df2
tgonzalezdiuk <tamara.gsx@gmail.com>
2024-03-25 14:59:39 -0400

    14:59

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+/** Tamara Gonzalez
+ * Date : March 25th, 2024 
+ * Program : Java
+ */
+
+ import java.*;
+
+ public class Factorial 
+ {
+     public static void main(String[] args)
+     {
+         Scanner input = new Scanner(System.in);
+         System.out.println("Enter a number");
+         int Number = input.nextInt();
+
+         int Factorial = 
+
+         for (int i = 1; i <= Number, i ++)
+         Factotrial = Number*i
+
+
+     }
+ }
